pf bugfix : occupancy field indexing, particle update logic, best-particle computation logic, weighted probabilistic temporal smoothing

robot_localizer/scripts/pf.py
@@ -70,7 +70,7 @@ class ParticleFilterROS(object):
         self.pf_.initialize(
                 size = 1000,
                 seed = xy_theta,
-                spread = [0.2, 0.5]
+                spread = [0.4, 0.5]
                 )
 
         ps = self.pf_.particles
@@ -102,7 +102,7 @@ class ParticleFilterROS(object):
 
             # inverse frame computation
             dx,dy,dh = delta
-            c, s = np.cos(delta[2]), np.sin(delta[2])
+            c, s = np.cos(self.last_odom_[2]), np.sin(self.last_odom_[2])
             delta[0] =  c*dx + s*dy
             delta[1] = -s*dx + c*dy
             delta[2] = U.anorm(dh)
@@ -113,15 +113,22 @@ class ParticleFilterROS(object):
         best = None
         if scan is not None:
             ps = self.pf_.particles
-            if np.linalg.norm(self.delta_[:2]) > 0.3 or self.delta_[2] > np.deg2rad(30):
-                # moved 10cm or 10 deg
+            if np.linalg.norm(self.delta_[:2]) > 0.1 or self.delta_[2] > np.deg2rad(10):
+                # only update moved 10cm or 10 deg
+
+                # reset delta
+                self.delta_ *= 0
+
                 ws = self.pm_.match(self.pf_.particles, scan)
                 #print('ws', ws)
                 #plt.scatter(ps[:,0], ps[:,1], label='resample', s=ws, alpha=1.0)
-                #plt.show()
-                best = self.pf_.resample(ws, noise=[0.01,0.01,0.01])
 
-        self.rb_.publish(self.pf_.particles, best)
+                #plt.show()
+                best = self.pf_.resample(ws, noise=[0.05,0.05,0.01])
+
+        good_idx = np.argsort(self.pf_.weights_)[::-1]
+
+        self.rb_.publish(self.pf_.particles[good_idx[:500]], best)
 
         if best is not None:
             x,y,h = best

robot_localizer/src/robot_localizer/particle_filter.py
@@ -10,6 +10,8 @@ class ParticleFilter(object):
         # general parameters
         self.size_ = None
         self.particles_ = None
+        self.weights_ = None
+        self.gamma_ = (1.0/8.0)
 
         # initialization parameters
         self.radius = 10 #the limit of how big the particle field is generated.
@@ -57,6 +59,7 @@ class ParticleFilter(object):
         h = U.anorm(h)
 
         self.particles_ = np.stack([x,y,h], axis=-1)
+        self.weights_ = np.full(size, 1.0/size)
         self.size_ = size
 
         #for i in range(size):
@@ -117,12 +120,11 @@ class ParticleFilter(object):
             plt.title('weight')
 
         if inv:
-            prob = 1.0 / (np.add(weight, eps))
-        else:
-            prob = weight
+            weight = 1.0 / (np.add(weight, eps))
 
-        prob[np.isnan(prob)] = 0.0
-        prob /= np.sum(prob)
+        # weight rectification
+        weight[np.isnan(weight)] = 0.0
+        weight /= np.sum(weight)
 
         if viz:
             plt.figure()
@@ -140,15 +142,21 @@ class ParticleFilter(object):
         #particles = self.particles_[idx]
 
         # "perfect" resampler
-        print 'prob', np.min(prob), np.max(prob), np.std(prob)
-        self.particles_, ws = resample(self.particles_, prob)
+        print 'weight statistics', weight.min(), weight.max(), weight.std() #np.min(weight), np.max(weight), np.std(weight)
+        weight = (self.gamma_ * self.weights_) + (1.0 - self.gamma_) * weight
+        weight /= weight.sum()
+
+        self.particles_, self.weights_ = resample(self.particles_, weight)
+        self.weights_ /= self.weights_.sum()
+
         #self.particles_ += np.random.normal(loc=0.0, scale=noise)
         self.particles_ = np.random.normal(loc=self.particles_, scale=noise)
 
         # TODO : better "best" particle
-
-        x,y = np.mean(self.particles_[:,:2], axis=0)
-        h = U.amean(self.particles[:,2])
+        x, y = np.average(self.particles[:,:2], axis=0, weights=self.weights_)
+        #x, y = np.sum(self.particles_[:,:2] * self.weights_[:,nax], axis=0)
+        #x, y = x/self.size_, y/self.size_
+        h = U.amean(self.particles[:,2], w=self.weights_)
         return [x,y,h]
 
         #return self.particles_[np.argmax(ws)]

robot_localizer/src/robot_localizer/particle_matcher.py
@@ -15,13 +15,16 @@ class ParticleMatcher(object):
         #scan is a list of [angle, theta]
         min_dist = np.min(scan[:,1])
         #print('ps', particle_list)
-        dist = [self.OF.get_closest_obstacle_distance(p[0], p[1]) for p in particle_list]
+        #dist = [self.OF.get_closest_obstacle_distance(p[0], p[1]) for p in particle_list]
+        ps = particle_list
+        dist = self.OF.get_closest_obstacle_distance(ps[:,0], ps[:,1])
         dist = np.asarray(dist, dtype=np.float32)
+        print('dist stats : min {} max {} std {}'.format(dist.min(),dist.max(),dist.std()))
 
         cost = np.abs(np.subtract(dist, min_dist))
         cost[np.isnan(cost)] = 0 # set nan cost to zero to prevent artifacts
         #weight = cost.max() - cost + 0.01
-        weight = 1.0 / (cost + 1e-02)
+        weight = 1.0 / cost
         weight[np.isnan(dist)] = 0 # set nan weight to zero to make sure it doesn't get sampled
         #print('ws', weight)
         return weight